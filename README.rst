dba82c9de0413691af6e3b02409149ae8a01292c
Michel <michel@caravan.coop>
2016-12-10 11:08:18 -0500

    refactoring of main/bootstrap files

--- README.rst
+++ README.rst
@@ -2,3 +2,12 @@ Flask Admin demo
 ================
 
 Flask setup with some extension to compare with  django capabilities.
+
+Roadmap
+=======
+
+1. tests
+2. flask-admin setup
+3. assets and static config
+4. babel setup
+5. flask-alembic setup and first migrations
--- src/main.py
+++ src/main.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python
 
-from flask import render_template
+from flask import render_template, Flask
+from flask_script import Manager, Server
+from flask_mail import Mail, email_dispatched
+from flask_user import UserManager, SQLAlchemyAdapter
 
-from bootstrap import app, manager
+from models import db, User
+
+
+app = Flask(__name__)
+
+app.config.from_pyfile("config/default.cfg")
+app.config.from_envvar("ENV_CONFIG_FILE", silent=True)
+
+db.init_app(app)
+# db.create_all()
+user_db_adapter = SQLAlchemyAdapter(db, User)
+
+mail = Mail(app)
+user_manager = UserManager(user_db_adapter, app)
+# admin = Admin(app, name="Flask", template_mode="bootstrap3")
+
+manager = Manager(app)
+manager.add_command("runserver", Server(
+    host="0.0.0.0",
+    port=5000,
+))
+
+
+@email_dispatched.connect
+def log_message(message, app):
+    """Provides console display for sent emails
+    """
+    if app.config.get("DEBUG", False):
+        log = [message.subject, "_" * len(message.subject), message.body]
+        app.logger.debug("\n".join(log))
 
 
 @app.route("/")
